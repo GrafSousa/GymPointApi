2a6e0551476ff54eb329998f49eddcbc9a69dc46
Gustavo <graf.sousa@gmail.com>
2019-10-24 19:55:13 -0300

    Adiciona validação do dados ao cadastrar estudante

--- src/app/controllers/StudentController.ts
+++ src/app/controllers/StudentController.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
 
 import Student from '../models/Student';
 
 class StudentController {
   public async store(req: Request, res: Response): Promise<Response> {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(409).json({ error: 'Student already exists' });
+    }
+
     const { id, name, email, age, weight, height } = await Student.create(
       req.body
     );
