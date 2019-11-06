12104a0be18bc4ad5d0360d26e989c1aa379d52f
Gustavo <graf.sousa@gmail.com>
2019-11-05 22:09:29 -0300

    Adiciona feature de tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
-import express from 'express';
+import express, {
+  Request,
+  Response,
+  ErrorRequestHandler,
+  NextFunction,
+} from 'express';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 
 import { i18n } from './i18n';
+import 'express-async-errors';
 import { routes } from './routes/index';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -11,21 +20,41 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
     this.i18n();
+    this.exceptionHandler();
   }
 
   private middlewares(): void {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
   }
 
   private routes(): void {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   private i18n(): void {
     this.server.use(i18n.init);
+  }
+
+  private exceptionHandler(): void {
+    this.server.use(
+      async (
+        err: ErrorRequestHandler,
+        req: Request,
+        res: Response,
+        next: NextFunction
+      ) => {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+    );
   }
 }
 
---
+++ src/config/sentry.ts
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://12cf4551caa24414a3f7ae2909f20a17@sentry.io/1809191',
+};
