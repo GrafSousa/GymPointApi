ae7061a03cbe505a4909047a4199a573be391439
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2019-11-07 10:22:15 -0300

    Refatora EnrollMentController

--- src/app/controllers/EnrollmentController.ts
+++ src/app/controllers/EnrollmentController.ts
@@ -1,235 +1,163 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import * as Yup from 'yup';
 import { Request, Response } from 'express';
+import * as Yup from 'yup';
 import { parseISO, isBefore, addMonths, startOfDay } from 'date-fns';
-import { Op } from 'sequelize';
 
-import { Plan } from '../models/Plan';
-import { Student } from '../models/Student';
+import Queue from '../../lib/Queue';
+import EnrollMail from '../jobs/EnrollMail';
 import { Enrollment } from '../models/Enrollment';
 
-import EnrollMail from '../jobs/EnrollMail';
-import Queue from '../../lib/Queue';
-
 import { i18n } from '../../i18n';
+import { HttpApiException, BadRequestApiException } from '../errors/index';
+
+import PlanServiceImpl from '../services/PlanServiceImpl';
+import StudentServiceImpl from '../services/StudentServiceImpl';
+import EnrollmentServiceImpl from '../services/EnrollmentServiceImpl';
 
 class EnrollmentController {
   async index(req: Request, res: Response): Promise<Response> {
     const { page = 1 } = req.query;
-    const enrollments = await Enrollment.findAll({
-      where: {
-        canceled_at: null,
-      },
-      attributes: ['id', 'start_date', 'end_date', 'price'],
-      limit: 20,
-      offset: (page - 1) * 20,
-      include: [
-        {
-          model: Student,
-          as: 'student',
-          attributes: ['id', 'name'],
-        },
-        {
-          model: Plan,
-          as: 'plan',
-          attributes: ['id', 'title', 'duration', 'price'],
-        },
-      ],
-    });
+    const enrollments = await Enrollment.findAllNotCanceled(page);
     return res.json(enrollments);
   }
 
   async store(req: Request, res: Response): Promise<Response> {
-    const schema = Yup.object().shape({
-      student_id: Yup.number().required(),
-      plan_id: Yup.number().required(),
-      start_date: Yup.date().required(),
-    });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: `${i18n.__('validation.fail')}` });
-    }
+    this.verifyRequest(req);
 
     const { plan_id, student_id, start_date } = req.body;
+    try {
+      const startDay = this.calculateStardDay(start_date);
 
-    const plan = await Plan.findOne({
-      where: { id: plan_id, excluded: false },
-    });
+      if (isBefore(startDay, startOfDay(new Date()))) {
+        return res
+          .status(400)
+          .json({ error: `${i18n.__('enrollment.pastDates')}` });
+      }
 
-    if (!plan) {
-      return res.status(404).json({ error: `${i18n.__('plan.notFound')}` });
+      const plan = await PlanServiceImpl.findPlanOrThrow(plan_id);
+      const student = await StudentServiceImpl.findStudentOrThrow(student_id);
+
+      const end_date = this.calculateEndDate(start_date, plan.duration);
+
+      await EnrollmentServiceImpl.isEnrollmentOverlapping(startDay, end_date);
+
+      const price = this.calculatePrice(plan.duration, plan.price);
+      const enrollment = await Enrollment.create({
+        plan_id,
+        student_id,
+        start_date,
+        end_date,
+        price,
+      });
+
+      await Queue.add(EnrollMail.key, {
+        student,
+        enrollment,
+      });
+
+      return res.json(enrollment);
+    } catch (e) {
+      if (e instanceof HttpApiException) {
+        return res.status(e.code).json(e.message);
+      }
+      return res.json(e.message);
     }
-
-    const student = await Student.findByPk(student_id);
-
-    if (!student) {
-      return res.status(404).json({ error: `${i18n.__('student.notFound')}` });
-    }
-
-    const startDay = startOfDay(new Date(start_date));
-
-    if (isBefore(startDay, startOfDay(new Date()))) {
-      return res
-        .status(400)
-        .json({ error: `${i18n.__('enrollment.pastDates')}` });
-    }
-
-    const end_date = addMonths(parseISO(startDay.toISOString()), plan.duration);
-    const price = plan.duration * plan.price;
-
-    const existsEnrollment = await Enrollment.count({
-      where: {
-        canceled_at: null,
-        [Op.and]: {
-          start_date: {
-            [Op.lte]: end_date,
-          },
-          end_date: {
-            [Op.gte]: startDay,
-          },
-        },
-      },
-    });
-
-    if (existsEnrollment > 0) {
-      return res
-        .status(409)
-        .json({ error: `${i18n.__('enrollment.overlapping')}` });
-    }
-
-    const enrollment = await Enrollment.create({
-      plan_id,
-      student_id,
-      start_date,
-      end_date,
-      price,
-    });
-
-    await Queue.add(EnrollMail.key, {
-      student,
-      enrollment,
-    });
-
-    return res.json(enrollment);
   }
 
   async update(req: Request, res: Response): Promise<Response> {
-    const schema = Yup.object().shape({
+    this.verifyRequest(req);
+
+    const { plan_id, student_id, start_date } = req.body;
+    try {
+      await StudentServiceImpl.existsStudent(student_id);
+
+      const enrollment = await EnrollmentServiceImpl.findEnrollmentOrThrow(
+        req.body.id
+      );
+
+      this.isStudentOfThisEnrollment(
+        enrollment.student_id,
+        req.body.student_id
+      );
+
+      const plan = await PlanServiceImpl.findPlanOrThrow(plan_id);
+      const startDay = this.calculateStardDay(start_date);
+      const end_date = this.calculateEndDate(start_date, plan.duration);
+
+      await EnrollmentServiceImpl.isEnrollmentOverlapping(startDay, end_date);
+      await Enrollment.deleteEnrollment(enrollment);
+
+      const price = this.calculatePrice(plan.duration, plan.price);
+      const newEnrollment = await Enrollment.create({
+        plan_id,
+        student_id,
+        start_date,
+        end_date,
+        price,
+      });
+
+      return res.json(newEnrollment);
+    } catch (e) {
+      if (e instanceof HttpApiException) {
+        return res.status(e.code).json(e.message);
+      }
+      return res.json(e.message);
+    }
+  }
+
+  async delete(req: Request, res: Response): Promise<Response> {
+    const enrollment = await Enrollment.findOneNotCanceled(req.params.id);
+
+    this.isStudentOfThisEnrollment(enrollment.student_id, req.body.student_id);
+
+    await Enrollment.deleteEnrollment(enrollment);
+
+    return res.json(enrollment);
+  }
+
+  async verifyRequest(req: Request): Promise<void> {
+    const schema = this.createSchema();
+
+    if (!(await schema.isValid(req.body))) {
+      throw new BadRequestApiException(`${i18n.__('validation.fail')}`);
+    }
+  }
+
+  createSchema() {
+    return Yup.object().shape({
       student_id: Yup.number().required(),
       plan_id: Yup.number().required(),
       start_date: Yup.date().required(),
     });
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: `${i18n.__('validation.fail')}` });
-    }
-
-    const { plan_id, student_id, start_date } = req.body;
-
-    const plan = await Plan.findOne({
-      where: { id: plan_id, excluded: false },
-    });
-
-    if (!plan) {
-      return res.status(404).json({ error: `${i18n.__('plan.notFound')}` });
-    }
-
-    const student = await Student.findByPk(student_id);
-
-    if (!student) {
-      return res.status(404).json({ error: `${i18n.__('student.notFound')}` });
-    }
-
-    const enrollment = await Enrollment.findOne({
-      where: {
-        id: req.params.id,
-        canceled_at: null,
-      },
-    });
-
-    if (!enrollment) {
-      return res
-        .status(404)
-        .json({ error: `${i18n.__('enrollment.notFound')}` });
-    }
-
-    if (enrollment.student_id !== req.body.student_id) {
-      return res
-        .status(400)
-        .json({ error: `${i18n.__('enrollment.invalidStudent')}` });
-    }
-
-    const startDay = startOfDay(new Date(start_date));
-    const end_date = addMonths(parseISO(start_date), plan.duration);
-
-    const existsEnrollment = await Enrollment.count({
-      where: {
-        canceled_at: null,
-        [Op.and]: {
-          start_date: {
-            [Op.lte]: end_date,
-          },
-          end_date: {
-            [Op.gte]: startDay,
-          },
-        },
-      },
-    });
-
-    if (existsEnrollment > 0) {
-      return res
-        .status(409)
-        .json({ error: `${i18n.__('enrollment.overlapping')}` });
-    }
-
-    enrollment.canceled_at = new Date();
-
-    await enrollment.save();
-
-    const price = plan.duration * plan.price;
-
-    const newEnrollment = await Enrollment.create({
-      plan_id,
-      student_id,
-      start_date,
-      end_date,
-      price,
-    });
-
-    return res.json(newEnrollment);
   }
 
-  async delete(req: Request, res: Response): Promise<Response> {
-    const enrollment = await Enrollment.findOne({
-      where: {
-        id: req.params.id,
-        canceled_at: null,
-      },
-    });
+  calculateStardDay(start_date: string): Date {
+    return startOfDay(new Date(start_date));
+  }
 
-    if (!enrollment) {
-      return res
-        .status(404)
-        .json({ error: `${i18n.__('enrollment.notFound')}` });
+  calculateEndDate(start_date: string, duration: number): Date {
+    return addMonths(parseISO(start_date), duration);
+  }
+
+  calculatePrice(duration: number, price: number): number {
+    return duration * price;
+  }
+
+  isStudentOfThisEnrollment(
+    studentEnrollmentId: number,
+    reqStudentId: number
+  ): void {
+    if (studentEnrollmentId !== reqStudentId) {
+      throw new BadRequestApiException(
+        `${i18n.__('enrollment.invalidStudent')}`
+      );
     }
+  }
 
-    if (enrollment.student_id !== req.body.student_id) {
-      return res
-        .status(400)
-        .json({ error: `${i18n.__('enrollment.invalidStudent')}` });
+  isPlanOfThisEnrollment(planEnrollmentId: number, reqPlanId: number): void {
+    if (planEnrollmentId !== reqPlanId) {
+      throw new BadRequestApiException(`${i18n.__('enrollment.invalidPlan')}`);
     }
-
-    if (enrollment.plan_id !== req.body.plan_id) {
-      return res
-        .status(400)
-        .json({ error: `${i18n.__('enrollment.invalidPlan')}` });
-    }
-
-    enrollment.canceled_at = new Date();
-
-    await enrollment.save();
-
-    return res.json(enrollment);
   }
 }
 
